827713216e1caae92a28e2d82f41c00c71ce2737
KynanPR <kynanpr@gmail.com>
2025-06-24 16:32:04 +0100

    Basic startpoint

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -117,8 +117,8 @@ const RootLayoutNav = () => {
             ),
           }}
         >
-          <Stack.Screen name="(auth)" options={{ headerShown: false }} />
           <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="(auth)" options={{ headerShown: false }} />
           <Stack.Screen name="drawer" options={{ headerShown: false }} />
           <Stack.Screen
             name="search"
